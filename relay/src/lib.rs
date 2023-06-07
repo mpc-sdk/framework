e191eda76e558b2080fdcb9231a2ed29b9cc040c
muji <muji@tmpfs.org>
2023-06-07 18:09:36 +0800

    Move basic noise transport test spec.

--- relay/src/lib.rs
+++ relay/src/lib.rs
@@ -4,7 +4,6 @@ mod error;
 pub mod keypair;
 mod protocol;
 mod server;
-mod transport;
 
 pub use client::{ClientOptions, EventLoop, NativeClient};
 pub use error::Error;
--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -3,3 +3,5 @@ mod test_utils;
 
 #[cfg(not(target_arch = "wasm32"))]
 mod handshake;
+
+mod noise_transport;
