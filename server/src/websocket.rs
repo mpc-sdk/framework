7c20b4c54724548e522cacf71da040a2b8f33336
muji <muji@tmpfs.org>
2023-06-17 10:33:38 +0800

    Tidy debug statement.

--- server/src/websocket.rs
+++ server/src/websocket.rs
@@ -176,7 +176,6 @@ async fn read(
             Ok(msg) => match msg {
                 Message::Text(_) => {}
                 Message::Binary(buffer) => {
-                    //println!("server ws read buffer");
                     tx.send(buffer).await?;
                 }
                 Message::Ping(_) => {}
