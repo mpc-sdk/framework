8a4021375d4419e061baa64be1d44aa5f6f57549
muji <muji@tmpfs.org>
2023-10-21 12:55:28 +0800

    Add compression helpers behind zlib flag.

---
+++ protocol/src/zlib.rs
@@ -0,0 +1,34 @@
+//! Compression helpers using zlib.
+
+use flate2::{write::{ZlibEncoder, ZlibDecoder}, Compression};
+use std::io::prelude::*;
+use crate::Result;
+
+/// Compress bytes.
+pub fn deflate(packet: &[u8]) -> Result<Vec<u8>> {
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::fast());
+    encoder.write_all(packet)?;
+    Ok(encoder.finish()?)
+}
+
+/// Decompress bytes.
+pub fn inflate(packet: &[u8]) -> Result<Vec<u8>> {
+    let mut z = ZlibDecoder::new(Vec::new());
+    z.write_all(packet)?;
+    Ok(z.finish()?)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use anyhow::Result;
+
+    #[test]
+    fn compress_decompress() -> Result<()> {
+        let packet = "Some message that we send out over the wire.";
+        let compressed = deflate(packet.as_bytes())?;
+        let decompressed = inflate(&compressed)?;
+        assert_eq!(packet.as_bytes(), &decompressed);
+        Ok(())
+    }
+}
