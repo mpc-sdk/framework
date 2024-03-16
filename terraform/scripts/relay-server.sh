65f01dc9debcec9f7ae6fdab71edd5ea691e8a74
muji <muji@tmpfs.org>
2024-03-16 13:45:44 +0800

    Specify package argument.

--- terraform/scripts/relay-server.sh
+++ terraform/scripts/relay-server.sh
@@ -6,7 +6,7 @@ sudo -u ec2-user -i <<'EOF'
 ROOT=$(pwd)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
-cargo install --git https://github.com/mpc-sdk/framework.git
+cargo install --git https://github.com/mpc-sdk/framework.git mpc-relay
 mpc-relay generate-keypair -f server.pem
 echo 'key = "server.pem"' > config.toml
 sudo mpc-relay start config.toml --bind 0.0.0.0:80
