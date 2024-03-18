dc370820de397f131a06136e685b6fa0bbe1008b
muji <muji@tmpfs.org>
2024-03-18 09:11:49 +0800

    Update startup script for CORS.

--- terraform/scripts/relay-server.sh
+++ terraform/scripts/relay-server.sh
@@ -9,5 +9,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install --git https://github.com/mpc-sdk/framework.git mpc-relay
 mpc-relay generate-keypair -f server.pem
 echo 'key = "server.pem"' > config.toml
+echo '[cors]' >> config.toml
+echo 'origins = ["https://tss.ac"]' >> config.toml
 sudo $ROOT/.cargo/bin/mpc-relay start config.toml --bind 0.0.0.0:80 &
 EOF
