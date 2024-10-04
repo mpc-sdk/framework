355090e095427bd652f19ed67172ccecbaa33a87
muji <muji@tmpfs.org>
2024-10-04 22:09:38 +0800

    Bump rustc_version.

--- driver/build.rs
+++ driver/build.rs
@@ -1,6 +1,8 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(CHANNEL_NIGHTLY)");
+
     // Set cfg flags depending on release channel
     let channel = match version_meta().unwrap().channel {
         Channel::Stable => "CHANNEL_STABLE",
