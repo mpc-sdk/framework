06d5352b2c738a4bd967a3e4a918ffc8377e55e0
muji <muji@tmpfs.org>
2024-10-23 14:05:52 +0800

    Tidy test spec.

--- conformance/signers/webassembly-eddsa/main.js
+++ conformance/signers/webassembly-eddsa/main.js
@@ -1,33 +1,31 @@
 import init, { EddsaSigner } from '@eddsa-bindings';
 
+await init();
+
+console.log("webassembly initialized...");
+
 function stringToUint8Array(str) {
   const encoder = new TextEncoder();
   return encoder.encode(str);
 }
 
-init().then((module) => {
-  console.log("webassembly initialized...");
-  let signingKeyBytes = EddsaSigner.random();
-  console.log("signingKeyBytes", signingKeyBytes)
-  let signer = new EddsaSigner(signingKeyBytes);
-  console.log("verifyingKey", signer.verifyingKey());
+let signingKeyBytes = EddsaSigner.random();
+let signer = new EddsaSigner(signingKeyBytes);
+let messageBytes = stringToUint8Array("example message to sign");
+let signature = signer.sign(messageBytes);
 
-  let message = "example message to sign";
-  let messageBytes = stringToUint8Array(message);
+console.log("signingKeyBytes", signingKeyBytes)
+console.log("verifyingKey", signer.verifyingKey());
+console.log("signature", signature);
 
-  let signature = signer.sign(messageBytes);
-  console.log("signature", signature);
+let verified = false;
 
-  let verified = false;
-  
-  try {
-    signer.verify(messageBytes, signature);
-    verified = true;
-    console.log("signature verified");
-  } catch (e) {
-    console.error("verification failed", e);
-  }
+try {
+  signer.verify(messageBytes, signature);
+  verified = true;
+  console.log("signature verified");
+} catch (e) {
+  console.error("verification failed", e);
+}
 
-  console.assert(verified);
-
-})
+console.assert(verified);
