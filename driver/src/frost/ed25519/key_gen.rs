c738360dec6ec53ad20d78b35ca68f056652c5fe
muji <muji@tmpfs.org>
2024-11-08 11:56:26 +0800

    Prepare constructor and identifiers for keygen.

--- driver/src/frost/ed25519/key_gen.rs
+++ driver/src/frost/ed25519/key_gen.rs
@@ -1,10 +1,14 @@
 //! Key generation for FROST Ed25519.
 use async_trait::async_trait;
-use frost_ed25519::keys::{KeyPackage, PublicKeyPackage};
+use frost_ed25519::{
+    keys::{dkg, KeyPackage, PublicKeyPackage},
+    Identifier,
+};
+use k256::ecdsa::{SigningKey, VerifyingKey};
 use mpc_client::{Event, NetworkTransport, Transport};
 use mpc_protocol::{hex, SessionId, SessionState};
 use rand::rngs::OsRng;
-use std::collections::BTreeSet;
+use std::{collections::BTreeMap, num::NonZeroU16};
 
 use crate::{
     frost::{Error, Result},
@@ -19,13 +23,42 @@ pub struct KeyGenDriver {
 }
 
 impl KeyGenDriver {
-    /// Create a new CGGMP key generator.
+    /// Create a new FROST key generator.
     pub fn new(
         transport: Transport,
         session: SessionState,
         session_id: SessionId,
+        max_signers: u16,
+        min_signers: u16,
+        identifiers: Vec<Identifier>,
+        signer: SigningKey,
+        verifiers: Vec<VerifyingKey>,
     ) -> Result<Self> {
-        todo!();
+        let party_number = session
+            .party_number(transport.public_key())
+            .ok_or_else(|| {
+                Error::NotSessionParticipant(hex::encode(
+                    transport.public_key(),
+                ))
+            })?;
+
+        let driver = FrostDriver::new(
+            session_id,
+            party_number,
+            max_signers,
+            min_signers,
+            identifiers,
+            // signer,
+            // verifiers,
+        )?;
+
+        let bridge = Bridge {
+            transport,
+            driver: Some(driver),
+            session,
+            party_number,
+        };
+        Ok(Self { bridge })
     }
 }
 
@@ -57,12 +90,34 @@ impl From<KeyGenDriver> for Transport {
 }
 
 /// FROST keygen driver.
-struct FrostDriver {}
+struct FrostDriver {
+    session_id: SessionId,
+    party_number: NonZeroU16,
+    max_signers: u16,
+    min_signers: u16,
+    identifiers: Vec<Identifier>,
+
+    round1_packages: BTreeMap<Identifier, dkg::round1::SecretPackage>,
+}
 
 impl FrostDriver {
     /// Create a key generator.
-    pub fn new(session_id: SessionId) -> Result<Self> {
-        todo!();
+    pub fn new(
+        session_id: SessionId,
+        party_number: NonZeroU16,
+        max_signers: u16,
+        min_signers: u16,
+        identifiers: Vec<Identifier>,
+    ) -> Result<Self> {
+        Ok(Self {
+            session_id,
+            party_number,
+            max_signers,
+            min_signers,
+            identifiers,
+
+            round1_packages: BTreeMap::new(),
+        })
     }
 }
 
@@ -76,6 +131,14 @@ impl ProtocolDriver for FrostDriver {
     }
 
     fn proceed(&mut self) -> Result<Vec<Self::Message>> {
+        /*
+        let (private_package, public_package) = dkg::part1(
+            participant_identifier,
+            self.max_signers,
+            self.min_signers,
+            &mut OsRng,
+        )?;
+        */
         todo!();
     }
 
--- driver/src/frost/ed25519/mod.rs
+++ driver/src/frost/ed25519/mod.rs
@@ -1,7 +1,86 @@
-//! Driver for the FROST Ed25519 protocol.\
+//! Driver for the FROST Ed25519 protocol.
+use frost_ed25519::{
+    keys::{KeyPackage, PublicKeyPackage},
+    Identifier,
+};
 
 mod key_gen;
-
 pub use key_gen::KeyGenDriver;
 
+use super::Error;
+use mpc_client::{NetworkTransport, Transport};
+use mpc_protocol::SessionId;
+
+use crate::{
+    new_client, wait_for_close, wait_for_driver, wait_for_session,
+    wait_for_session_finish, Participant, SessionHandler,
+    SessionInitiator, SessionOptions, SessionParticipant,
+};
+
 type MessageOut = ();
+
+/// Run threshold DKG for the FROST protocol.
+pub async fn keygen(
+    options: SessionOptions,
+    participant: Participant,
+    session_id: SessionId,
+) -> crate::Result<(KeyPackage, PublicKeyPackage)> {
+    let n = options.parameters.parties;
+    let t = options.parameters.threshold;
+
+    // Create the client
+    let (client, event_loop) = new_client(options).await?;
+
+    let mut transport: Transport = client.into();
+
+    // Handshake with the server
+    transport.connect().await?;
+
+    // Start the event stream
+    let mut stream = event_loop.run();
+
+    // Wait for the session to become active
+    let client_session = if participant.party().is_initiator() {
+        let mut other_participants =
+            participant.party().participants().to_vec();
+        other_participants
+            .retain(|p| p != participant.party().public_key());
+        SessionHandler::Initiator(SessionInitiator::new(
+            transport,
+            other_participants,
+        ))
+    } else {
+        SessionHandler::Participant(SessionParticipant::new(
+            transport,
+        ))
+    };
+
+    let (transport, session) =
+        wait_for_session(&mut stream, client_session).await?;
+
+    let verifiers = participant
+        .party()
+        .verifiers()
+        .iter()
+        .cloned()
+        .collect::<Vec<_>>();
+
+    let mut identifiers: Vec<Identifier> =
+        Vec::with_capacity(n.into());
+    for index in 1..=n {
+        identifiers.push(index.try_into().map_err(Error::from)?);
+    }
+
+    let key_gen = KeyGenDriver::new(
+        transport,
+        session,
+        session_id,
+        n,
+        t,
+        identifiers,
+        participant.signing_key().to_owned(),
+        verifiers,
+    )?;
+
+    todo!();
+}
