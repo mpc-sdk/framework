65d2d2ce2e16378a6596043101080221ae1aff20
muji <muji@tmpfs.org>
2024-11-26 10:18:05 +0800

    Use protocols helper in node FROST tests.

--- conformance/protocols/node-frost-secp256k1-tr/tests/frost.spec.js
+++ conformance/protocols/node-frost-secp256k1-tr/tests/frost.spec.js
@@ -1,35 +1,25 @@
 import {
   Worker, isMainThread, parentPort, workerData,
 } from 'node:worker_threads';
-import fs from 'fs';
+
+import {
+  server,
+  parameters,
+  frost,
+} from '../../../helpers/protocols.mjs';
+const { message } = frost;
+
+// Node requires an object with id field for the bindings
+const identifiers = frost.identifiers.map((id) => {
+  return { id };
+});
 
 const dkgScript = './tests/dkg.js';
 const signScript = './tests/sign.js';
 
-// Convert from a hex-encoded string.
-function fromHexString(hex) {
-  return new Uint8Array(hex.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
-}
-
-const serverKey = fs.readFileSync(
-  "../../../crates/integration_tests/tests/server_public_key.txt", "utf8");
-
-const server = {
-  serverUrl: "ws://127.0.0.1:8008",
-  serverPublicKey: Array.from(fromHexString(serverKey)),
-};
-const parameters = {
-  parties: 3,
-  threshold: 2,
-};
-
 console.log("FROST Secp256k1 Taproot, begin dkg...");
 
 let tasks = [];
-
-const identifiers = Array.apply(null, Array(parameters.parties)).map((_, i) => {
-  return {id: i + 1};
-});
 
 for (let i = 0;i < parameters.parties;i++) {
   tasks.push(new Promise((resolve, reject) => {
@@ -52,12 +42,9 @@ for (let i = 0;i < parameters.parties;i++) {
 }
 
 const keyShares = await Promise.all(tasks);
-
 console.assert(keyShares.length === parameters.parties);
 
 console.log("FROST Secp256k1 Taproot, dkg complete, begin signing...");
-
-const message = "a3e6e406aeb475f43aa762bb752a8f9d57b7fa327a2a53c7ae00b13f8d116b38";
 
 // Pick the signing parties
 tasks = [];
